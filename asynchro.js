37a7f19e6f40c5ea74d8c2c0c8ac85dfa43d2c20
halurabiyyu <halurabiyyu@gmail.com>
2024-01-07 21:31:18 +0700

    Add Asynchronus

---
+++ asynchro.js
@@ -0,0 +1,10 @@
+// Asynchronus
+console.log('selamat datang!');
+
+setTimeout(() => {
+    console.log("Terima kasih sudah mampir, silakan datang kembali!");
+}, 3000)
+
+console.log('Ada yang bisa dibantu?');
+
+// callback 
